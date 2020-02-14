d0693b4c06ee2e7851ec2e965c9f2010859ce669
Krista Rutz <rutzkri000@gmail.com>
2020-02-14 12:24:08 -0800

    scrape phone, website, and new patient boolean from doctor lists and display in the table

--- src/main.js
+++ src/main.js
@@ -12,9 +12,13 @@ async function asyncApiCall(search){
 }
 
 function getElements(response) {
-  for (let i = 0; i < response.data.length; i++){
-    console.log(response.data[i].profile);
-    displayInfo(response.data[i]);
+  if(response.data.length > 0){
+    for (let i = 0; i < response.data.length; i++){
+      console.log(response.data[i].profile);
+      displayInfo(response.data[i]);
+    }
+  } else {
+    $("#errorResult").text("Your search did not match any results.");
   }
   // console.log(response.data[1].profile.first_name);
   // console.log(response.data[2].profile.first_name);
@@ -30,30 +34,35 @@ function getElements(response) {
 }
 
 function displayInfo(doctor){
-  let newRow = `<tr class="${doctor.profile.slug}"><td>${doctor.profile.first_name} ${doctor.profile.last_name}</td><td>${doctor.profile.title}</td></tr>`;
+  let newRow = `<tr class="${doctor.profile.slug}"><td>${doctor.profile.first_name} ${doctor.profile.last_name}</td><td>${doctor.profile.title}</td><td>${doctor.practices[0].visit_address}</td><td>${doctor.practices[0].phones[0].number}</td><td>${doctor.practices[0].website}</td><td>${doctor.practices[0].accepts_new_patients}</td></tr>`;
   $("#resultsList").append(newRow);
 }
 
 $(document).ready(function() {
-  $("#devBtn").click(function(){
-    console.log("dev button clicked");
-    let searchTerms = new Search();
-    
-
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
     console.log("form submitted");
     $("#resultsList").text("");
+    $("#errorResult").text("");
+
     const state = "WA".toLowerCase();
     const city = "Seattle".toLowerCase();
-    
-    // const location = `${state}-${city}`;
-    // const queryTerm = "lisp";
-    // const specialtyUID = "speech-therapist";
-    // const gender;
-    // const sort = "best-match-asc";
-    // const limit = 10;
-    // const skip = 0;
+
+    const location = `${state}-${city}`;
+    const queryTerm = $("#userSymptom").val();
+    const specialtyUID = "speech-therapist";
+    const gender = "";
+    const sort = "best-match-asc";
+    const limit = 10;
+    const skip = 0;
+
+    let searchTerms = new Search(location, queryTerm, specialtyUID, gender, sort, skip, limit);
 
     asyncApiCall(searchTerms);
+  });
 
+  $("#devBtn").click(function(){
+    console.log("dev button clicked");
+    let searchTerms = new Search();  
   });
 });
