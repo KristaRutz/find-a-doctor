a2f62c0e6dd807f1461a282efe6b0eed8946375a
Krista Rutz <rutzkri000@gmail.com>
2020-02-14 10:30:05 -0800

    put the api doctor finding service into a separate script file

---
+++ src/doctor-service.js
@@ -0,0 +1,11 @@
+export class doctorService{
+  async asyncApiCall(){
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
