fec5454c17112f8d39027a4ecc4dc2245dff509c
Krista Rutz <rutzkri000@gmail.com>
2020-02-14 11:17:07 -0800

    update search terms function

--- src/main.js
+++ src/main.js
@@ -5,33 +5,47 @@ import './styles.css' ;
 import { DoctorService } from './doctor-service';
 import { Search } from './searchterms';
 
+async function asyncApiCall(){
+  let doctorList = new DoctorService();
+  let jsonifiedResponse = await doctorList.asyncApiCall(searchTerms);
+  getElements(jsonifiedResponse);
+}
+
+function getElements(response) {
+  console.log(response.data[0].profile.first_name);
+  console.log(response.data[1].profile.first_name);
+  console.log(response.data[2].profile.first_name);
+  console.log(response.data[3].profile.first_name);
+  console.log(response.data[4].profile.first_name);
+  console.log(response.data[5].profile.first_name);
+  console.log(response.data[6].profile.first_name);
+  console.log(response.data[7].profile.first_name);
+  console.log(response.data[8].profile.first_name);
+  console.log(response.data[9].profile.first_name);
+  //console.log(response.data[10].profile.first_name);
+  //console.log(response.data[11].profile.first_name);
+}
+
 $(document).ready(function() {
   $("#devBtn").click(function(){
-    let searchTerms = new Search();
     console.log("dev button clicked");
+    let searchTerms = new Search();
+    
+
+    console.log("form submitted");
+    const state = "WA".toLowerCase();
+    const city = "Seattle".toLowerCase();
+    
+    const location = `${state}-${city}`;
+    const query = "lisp";
+    const specialtyUID = "speech-therapist";
+    const gender;
+    const sort = "best-match-asc";
+    const limit = 10;
+    const skip = 0;
+
+
 
     asyncApiCall();
-
-    async function asyncApiCall(){
-      let doctorList = new DoctorService();
-      let jsonifiedResponse = await doctorList.asyncApiCall(searchTerms);
-      getElements(jsonifiedResponse);
-    }
-
-    function getElements(response) {
-      console.log(response.data[0].profile.first_name);
-      console.log(response.data[1].profile.first_name);
-      console.log(response.data[2].profile.first_name);
-      console.log(response.data[3].profile.first_name);
-      console.log(response.data[4].profile.first_name);
-      console.log(response.data[5].profile.first_name);
-      console.log(response.data[6].profile.first_name);
-      console.log(response.data[7].profile.first_name);
-      console.log(response.data[8].profile.first_name);
-      console.log(response.data[9].profile.first_name);
-      //console.log(response.data[10].profile.first_name);
-      //console.log(response.data[11].profile.first_name);
-    }
-
   });
 });
