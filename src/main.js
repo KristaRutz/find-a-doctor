1aebb15e0389d77c5b95e16d2b158a0c8d1b2578
Krista Rutz <rutzkri000@gmail.com>
2020-02-14 11:44:00 -0800

    implement a dynamic display function to insert the doctor data from the fetched list into a table in the browser

--- src/main.js
+++ src/main.js
@@ -5,25 +5,33 @@ import './styles.css' ;
 import { DoctorService } from './doctor-service';
 import { Search } from './searchterms';
 
-async function asyncApiCall(){
+async function asyncApiCall(search){
   let doctorList = new DoctorService();
-  let jsonifiedResponse = await doctorList.asyncApiCall(searchTerms);
+  let jsonifiedResponse = await doctorList.asyncApiCall(search);
   getElements(jsonifiedResponse);
 }
 
 function getElements(response) {
-  console.log(response.data[0].profile.first_name);
-  console.log(response.data[1].profile.first_name);
-  console.log(response.data[2].profile.first_name);
-  console.log(response.data[3].profile.first_name);
-  console.log(response.data[4].profile.first_name);
-  console.log(response.data[5].profile.first_name);
-  console.log(response.data[6].profile.first_name);
-  console.log(response.data[7].profile.first_name);
-  console.log(response.data[8].profile.first_name);
-  console.log(response.data[9].profile.first_name);
+  for (let i = 0; i < response.data.length; i++){
+    console.log(response.data[i].profile);
+    displayInfo(response.data[i]);
+  }
+  // console.log(response.data[1].profile.first_name);
+  // console.log(response.data[2].profile.first_name);
+  // console.log(response.data[3].profile.first_name);
+  // console.log(response.data[4].profile.first_name);
+  // console.log(response.data[5].profile.first_name);
+  // console.log(response.data[6].profile.first_name);
+  // console.log(response.data[7].profile.first_name);
+  // console.log(response.data[8].profile.first_name);
+  // console.log(response.data[9].profile.first_name);
   //console.log(response.data[10].profile.first_name);
   //console.log(response.data[11].profile.first_name);
+}
+
+function displayInfo(doctor){
+  let newRow = `<tr class="${doctor.profile.slug}"><td>${doctor.profile.first_name} ${doctor.profile.last_name}</td><td>${doctor.profile.title}</td></tr>`;
+  $("#resultsList").append(newRow);
 }
 
 $(document).ready(function() {
@@ -33,19 +41,19 @@ $(document).ready(function() {
     
 
     console.log("form submitted");
+    $("#resultsList").text("");
     const state = "WA".toLowerCase();
     const city = "Seattle".toLowerCase();
     
-    const location = `${state}-${city}`;
-    const query = "lisp";
-    const specialtyUID = "speech-therapist";
-    const gender;
-    const sort = "best-match-asc";
-    const limit = 10;
-    const skip = 0;
+    // const location = `${state}-${city}`;
+    // const queryTerm = "lisp";
+    // const specialtyUID = "speech-therapist";
+    // const gender;
+    // const sort = "best-match-asc";
+    // const limit = 10;
+    // const skip = 0;
 
+    asyncApiCall(searchTerms);
 
-
-    asyncApiCall();
   });
 });
