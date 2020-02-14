743ecaf4bed80f95aa106b9049d7b42519dacd87
Krista Rutz <rutzkri000@gmail.com>
2020-02-14 09:04:37 -0800

    write api call function and function to log API responses for development purposes

--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 import 'bootstrap' ; 
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
+
+$(document).ready(function() {
+  $("#devBtn").click(function(){
+    
+    asyncApiCall();
+
+    async function asyncApiCall(){
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    }
+
+    function getElements(response) {
+      console.log(response.data[0].practices[0].name);
+      console.log(response.data[0].practices[1].name);
+    }
+
+  });
+});
